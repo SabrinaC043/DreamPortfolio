cac376efc9320ec1481937c1d5c1d088d521220f
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2022-11-21 15:06:06 -0600

    uploaded new resume

--- src/pages/Resume/resume.js
+++ src/pages/Resume/resume.js
@@ -1,24 +1,24 @@
 
-import devResume from '../../assets/images/devResume.pdf';
-import resume from '../../assets/images/resume1.png';
+import devResume from '../../assets/images/resumeDev.pdf';
+import resume from '../../assets/images/resume2022.jpg';
 import "./resume.css";
 import { BsFillLightningFill } from "react-icons/bs";
 export default function Resume() {
 
     return (
         <div className="resumePage">
-            <div className="quote">
-                <i> "As long as I have air in my lungs, I pursue excellence and world changing feats. Even if I do not change the world, I change myself. I am great and beyond capable of reaching my goals."
-                    -Sabrina
-                </i>
-            </div>
             <section className="centerResume">
-
+                <a className="buttonResume" href={devResume} download="Sabrina Carrion software developer">
+                    <BsFillLightningFill /> Download Sabrina's Resume </a>
                 <div className="pdfResume">
                     <img className="resumeImg" src={resume} alt="sabrina resume"></img>
-                    <a className="buttonResume" href={devResume} download="Sabrina Carrion Software Developer Resume"><BsFillLightningFill /> Sabrina Software Developer</a>
-                </div>
 
+                </div>
+                <div className="quote">
+                    <i> " Discipline is the bridge between goals and accomplishment.”"
+                        -Mother Theresa
+                    </i>
+                </div>
             </section>
         </div>
 
