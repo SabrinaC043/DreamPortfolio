4e38cd6b06299f8246a0d92c0072dba8feb8eb33
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-02-10 10:35:23 -0600

    pages

--- src/components/Homebase/Main.js
+++ src/components/Homebase/Main.js
@@ -1,30 +1,24 @@
 import About from "../../pages/AboutSab/about";
 import Project from "../../pages/WorkProjects/project";
-
 import Resume from "../../pages/Resume/resume";
 
+
 export default function Main({ page }) {
-    // change this to a switch case 
+
     function render() {
         if (page === 'about') {
             return <About />
-        }
-        else if (page === 'projects') {
-
+        } else if (page === 'projects') {
             return <Project />
-        }
-        else if (page === 'resume') {
+        } else if (page === 'resume') {
             return <Resume />
-
         }
-
-
     }
-
 
     return (
         <>
             {render()}
         </>
     )
+
 }
