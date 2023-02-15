34784b5d5f6ea283e999cd2258bca97cd1a9e22a
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-02-15 10:33:41 -0600

    uploaded new resume

--- src/pages/Resume/resume.js
+++ src/pages/Resume/resume.js
@@ -1,6 +1,6 @@
 
 import devResume from '../../assets/images/sabrinadeveloperresume2023.pdf';
-import resume from '../../assets/images/sabrinacarriondeveloper1.png';
+import resume from '../../assets/images/devImgRes.png';
 import "./resume.css";
 import { BsFillLightningFill } from "react-icons/bs";
 export default function Resume() {
