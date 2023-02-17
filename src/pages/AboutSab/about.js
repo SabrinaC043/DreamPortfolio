156aa351221e9d6883fbdc1db43d039f8bce6e0c
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-02-17 11:34:23 -0600

    updating portfolio page not reflecting update

--- src/pages/AboutSab/about.js
+++ src/pages/AboutSab/about.js
@@ -18,17 +18,10 @@ export default function About() {
                 <div className="portfolioImg">
                     <img className="sabrina" src={sabrina} alt="sabrina" />
                 </div>
-                <section className="favorites m-2">
-                    <div className="dichotomy">
-                        <img src={dichotomy} alt="dichotomy of leadership book" width={100} />
-                    </div>
-                    <div className="findingMe">
-                        <img src={violadavis} alt="finding me autobiography" width={100} />
-                    </div>
-                </section>
+
                 <section className="introSummary">
                     <div className="about mt-3">
-                        <p>I am elated to share insight as to who Sabrina Carrion is and how my life journey has brought me into the striking career of Software Development. Over a decade of pharmaceutical technology startups and pharmaceutical experience, has prepared me for the level of excellence required to execute as a Software Developer. </p> <p>My career as a Learning and Development Specialist, granted the opportunity to collaborate with folks from the software development field. I was eager to unravel more about this exciting and creative industry. </p>
+                        <p>I am elated to share insight as to who Sabrina Carrion is and my striking career in Software Development. With over a decade of pharmaceutical technology and startup-experience, I have prepared for the level of excellence and accuracy required to execute as a Software Developer. </p> <p>My past career as a Learning and Development Specialist positioned me to collaborate with folks from the software development field. After viewing their codes and platform bugs, etc. I was eager to unravel more about this exciting and creative industry. </p>
                         <p>I discovered Software developers were always learning new information and practiced coding daily to hone their skills. This is what grabbed my heart and paved a new way for a great career. I believe in constantly learning and with a growth mindset, nothing is impossible! Dive into this app for more insight into my Software Development achievements.</p>
                     </div>
 
@@ -51,6 +44,15 @@ export default function About() {
                 </section>
             </div>
             <section className="core">
+                <section className="favorites my-2 mx-2">
+                    <div>
+                        {/* <h6>Lorem Ipsum </h6> */}
+
+                    </div>
+                    {/* <img src={dichotomy} alt="dichotomy of leadership book" width={100} />
+
+                    <img src={violadavis} alt="finding me autobiography" width={100} /> */}
+                </section>
                 <section className="mission">
                     <p>Mission</p>
                     <p> My mission is to contribute to society in a meaningful way by pursuing my passion as an application developer. </p>
